b22c1d3c938d502431ee14cfc5936b7d9f2bdcdd
Torres9 <Torres9Jin@gmail.com>
2014-02-22 10:17:59 -0800

    create the BorW cocos2d project

---
+++ BorW/BorW/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  BorW
+//
+//  Created by Torres Jin on 2/22/2014.
+//  Copyright Torres Jin 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ BorW/BorW/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BorW
+//
+//  Created by Torres Jin on 2/22/2014.
+//  Copyright Torres Jin 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
